2154c52982671e3c406aad3315eb2232407caff2
Terence <167604947+Johnsonbary@users.noreply.github.com>
2024-09-07 07:24:30 +0300

    Update exo_1.c

--- exo_1.c
+++ exo_1.c
@@ -1,35 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-int savoir(int);
-char *affichage_ip();
-char *affichage_mask();
-char *determine_mask();
-char *affichage_classe();
-void affichage_subnet(char *mask_adress, char *ip);
-void affichage_broadcast(char *adress_network,char *ip_mask);
-
-//ip
-//msr 
-//classe, @reseau, @broadcast
-
-int main()
-{
-    char *ip;
-    char *mask;
-    ip = affichage_classe();
-    mask = determine_mask();
-    affichage_subnet(mask, ip);
-    affichage_broadcast(ip, mask);
-    return 0;
-}
+#include "exo_1.h"
 
 char *affichage_classe()
 {
     char *ip;
     int decimal[4];
-    int count = 0;
     int open;
     try:
     ip = affichage_ip();
@@ -38,18 +12,17 @@ char *affichage_classe()
     {
         goto try;
     }
-    printf("\n###############################################################################################\n\n");
     if ((decimal[0]>= 0 && decimal[0]<= 127) && (decimal[1]>= 0 && decimal[1]<= 255) && (decimal[2]>= 0 && decimal[2]<= 255) && (decimal[3]>= 0 && decimal[3]<= 255))
     {
-        printf("Adresse Classe             :\t\t\tA\n");
+		affichage_classe_type('A');
     }
     else if ((decimal[0]>= 128 && decimal[0]<= 191) && (decimal[1]>= 0 && decimal[1]<= 255) && (decimal[2]>= 0 && decimal[2]<= 255) && (decimal[3]>= 0 && decimal[3]<= 255))
     {
-        printf("Adresse Classe             :\t\t\tB\n");
+        affichage_classe_type('B');
     }
     else if ((decimal[0]>= 192 && decimal[0]<= 223) && (decimal[1]>= 0 && decimal[1]<= 255) && (decimal[2]>= 0 && decimal[2]<= 255) && (decimal[3]>= 0 && decimal[3]<= 255))
     {
-        printf("Adresse Classe             :\t\t\tC\n");
+        affichage_classe_type('C');
     }
     else
     {
@@ -62,18 +35,16 @@ void affichage_subnet(char *mask_adress, char *ip)
     int bytemask[4];
     int byteip[4];
     int add[4];
-    char *address_net;
-
+    
     sscanf(mask_adress, "%d.%d.%d.%d", &bytemask[0], &bytemask[1], &bytemask[2], &bytemask[3]);
     sscanf(ip, "%d.%d.%d.%d", &byteip[0], &byteip[1], &byteip[2], &byteip[3]);
-
+	
     add[0] = byteip[0] & bytemask[0];
     add[1] = byteip[1] & bytemask[1];
     add[2] = byteip[2] & bytemask[2];
     add[3] = byteip[3] & bytemask[3];
-
-    sprintf(address_net, "%d.%d.%d.%d", add[0], add[1], add[2], add[3]);
-    printf("\nAdresse du Reseau          : \t\t\t%s\n", address_net);
+	
+    printf("\nAdresse du Reseau          : \t\t\t%d.%d.%d.%d\n", add[0], add[1], add[2], add[3]);
 }
 
 void affichage_broadcast(char *adress_network, char *ip_mask)
@@ -81,7 +52,6 @@ void affichage_broadcast(char *adress_network, char *ip_mask)
     int bytes[4];
     int bytesMask[4];
     int bytesBrad[4];
-    char *add_broad;
 
     sscanf(adress_network, "%d.%d.%d.%d", &bytes[0], &bytes[1], &bytes[2], &bytes[3]);
     sscanf(ip_mask, "%d.%d.%d.%d", &bytesMask[0], &bytesMask[1], &bytesMask[2], &bytesMask[3]);
@@ -96,8 +66,8 @@ void affichage_broadcast(char *adress_network, char *ip_mask)
     bytesBrad[2] = bytes[2] | bytesMask[2];
     bytesBrad[3] = bytes[3] | bytesMask[3];
 
-    sprintf(add_broad, "%d.%d.%d.%d", bytesBrad[0], bytesBrad[1], bytesBrad[2], bytesBrad[3]);
-    printf("\nAdresse Broadcast          : \t\t\t%s\n", add_broad);
+    printf("\nAdresse Broadcast          : \t\t\t%d.%d.%d.%d\n", bytesBrad[0], bytesBrad[1], bytesBrad[2], bytesBrad[3]);
+	printf("-----------------------------------------------------------------\n");
 }
 char *affichage_ip()
 {
@@ -119,8 +89,6 @@ char *determine_mask()
     char *mask;
     int open = 0;
     int decimal[4];
-    int sum = 0;
-    int i;
     int know;
     try:
     mask = affichage_mask();
@@ -129,32 +97,58 @@ char *determine_mask()
     {
         goto try;
     }
-    for(i = 0; i < 4; i++)
-    {
-        sum =  sum + decimal[i];
-    }
-    know = savoir(sum);
-    if(know = 0)
+    know = savoir(mask);
+    if(know == 0)
     {
         goto try;
     }
+    printf("\n\n-----------------------------------------------------------------\n");
+    printf("Number of user possible    : \t\t\t%d\n", (int)pow(2, know) - 2);
     return (mask);
 }
-int savoir(int n)
+int savoir(char *mask)
 {
-    int i;
-    int know = 0;
-    int base;
-    int tmp = 0;
-    for(i = 31; i >= 0; i--)
+	int i,
+		j;
+	int know = 0;
+    int tableau_mask[4] = {0};
+    int *binary_mask;
+
+    sscanf(mask, "%d.%d.%d.%d", &tableau_mask[0], &tableau_mask[1], &tableau_mask[2], &tableau_mask[3]);
+    for(i = 0; i < 4; i++)
     {
-        base = (int)pow(2,i % 8);
-        tmp = know;
-        know = n / base;
-        n = n % base;
-        if(know == 0 && tmp == 1)
-            return 1;
-    }
-    return 0;
+		if(0 > tableau_mask[i] || tableau_mask[i] > 255)
+			return 0;
+		binary_mask = binary(tableau_mask[i]);
+		for(j = 0; j < 8; j++)
+		{
+			if(binary_mask[j] == 0)
+				know++;
+			if(binary_mask[j] == 1 && know > 1)
+				return 0;
+		}
+
+	}
+
+	return (know);
+}
+void affichage_classe_type(char class)
+{
+	printf("\n\n-----------------------------------------------------------------\n");
+    printf("Adresse Classe             :\t\t\t%c\n", class);
+	printf("-----------------------------------------------------------------\n\n");
+}
+int *binary(int entier)
+{
+	int	j;
+	int base;
+	int *binary_mask = (int*)malloc(8 * sizeof(int));
+	for(j = 7; j >= 0; j--)
+	{
+		base = entier % 2;
+		entier = entier / 2;
+		binary_mask[j] = base;
+	}
+	return (binary_mask);
 }
 
